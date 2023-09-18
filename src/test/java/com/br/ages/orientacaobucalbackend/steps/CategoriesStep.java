c570b475ee42507c09dbaa783125fd40972d7732
jp <jphrmartins@gmail.com>
2023-09-18 11:07:04 -0300

    ajustes alguns testes

--- src/test/java/com/br/ages/orientacaobucalbackend/steps/CategoriesStep.java
+++ src/test/java/com/br/ages/orientacaobucalbackend/steps/CategoriesStep.java
@@ -5,21 +5,34 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class CategoriesStep extends CucumberStep {
 
     private List<String> response;
+    private List<String> categories;
 
     @Given("I have a list of categories")
     public void iHaveAListOfCategories() {
-        assert 0 == 0;
+        categories = List.of("gengiva", "labios", "dente", "lingua", "garganta", "todos");
     }
 
     @When("I list the categories")
     public void listCategories() {
-        this.response = List.of("gengiva", "labios", "dente", "lingua", "garganta", "todos");
+        response = new ArrayList<>(categories);
+    }
+
+    @When("I filter the list by type {string}")
+    public void iFilterTheListByType(String filterCategory) {
+        this.response = this.categories.stream().filter(x -> x.equals(filterCategory))
+                .collect(Collectors.toList());
+    }
+
+    @Then("I should only see the filtered category")
+    public void iShouldSeeTheFollowingCategory() {
+        assert this.response.size() == 1;
     }
 
     @Then("I should see the (.*) categories")
@@ -27,17 +40,6 @@ public class CategoriesStep extends CucumberStep {
         for (String expectedCategory : expectedCategories) {
             assert this.response.contains(expectedCategory);
         }
-    }
-
-    @When("I filter the list by type {string}")
-    public void iFilterTheListByType(String filterCategory) {
-        this.response = this.response.stream().filter(x -> x.equals(filterCategory))
-                .collect(Collectors.toList());
-    }
-
-    @Then("I should only see the filtered category")
-    public void iShouldSeeTheFollowingCategory() {
-        assert this.response.size() == 1;
     }
 
 }
--- src/test/resources/features/categories.feature
+++ src/test/resources/features/categories.feature
@@ -3,9 +3,9 @@ Feature: List Categories
     Scenario Outline: List all
         Given I have a list of categories
         When I list the categories
-        Then I should see the <expectedCategoriesList> categories
+        Then I should see the <expectedCategories> categories
         Examples:
-            | expectedCategoriesList |
+            | expectedCategories |
             | "gengiva", "labios", "dente", "lingua", "garganta", "todos" |
 
     Scenario Outline: List filtered categories
